133010cf0c09267203502260482081ec76e92178
Riley Wood <riley@clearmotion.com>
2017-03-04 11:09:15 -0500

    Better test bench, runs through random inputs
    Identified a problem where encrypted data can contain
    a newline which spoofs a delimiter. Need a better protocol
    without delimiters.

--- testme.py
+++ testme.py
@@ -1,40 +1,69 @@
 import serial
 import time
 import sys
+import unittest
+import random
+import string
 
 RESET_DELAY = 2
-DELAY = 0.1
+DELAY = 0.5
 
 
-def main():
-    s = serial.Serial("/dev/ttyUSB1", 115200)
-    time.sleep(RESET_DELAY)
-    input = "Riley"
+def encrypt(serial_port, input):
+    """
+    @brief      Encrypt a string with crypto box.
+    @warning    Can't encrypt newlines
 
-    print input
+    @param      serial_port  The serial port
+    @param      input        The input, no newline at the end
 
-    while True:
-        input_fmtd = "E"+input+"\n"
-        s.write(input_fmtd)
-        time.sleep(DELAY)
-        response = ""
-        while s.inWaiting():
+    @return     The input, encrypted
+    """
+    s = serial_port
+    s.write("E"+input+"\n")
+    time.sleep(DELAY)
+    response = ""
+    while s.inWaiting():
             response += s.read()
-        encrypted = response
-        print [x for x in encrypted]
+    return response
 
-        time.sleep(DELAY)
 
-        to_decrypt = "D"+encrypted+"\n"
-        print "Sent", s.write(to_decrypt), "of", len(to_decrypt)
-        time.sleep(DELAY)
-        response = ""
-        while s.inWaiting():
+def decrypt(serial_port, input):
+    """
+    @brief      Decrypt a string with crypto box.
+    @warning    Can't decrypt newlines
+
+    @param      serial_port  The serial port
+    @param      input        The input, no newline at the end
+
+    @return     The input, decrypted
+    """
+    s = serial_port
+    s.write("D"+input+"\n")
+    time.sleep(DELAY)
+    response = ""
+    while s.inWaiting():
             response += s.read()
-        decrypted = response
-        print [x for x in decrypted]
+    return response
 
-        assert decrypted==input
+
+class TestCryptoBox(unittest.TestCase):
+    def test_encrypt_then_decrypt_matches_input(self):
+        s = serial.Serial("/dev/ttyUSB0", 115200)
+        time.sleep(RESET_DELAY)
+
+        for i in xrange(0, 100):
+            rand_len = random.randint(1, 20)
+            input = ''.join(random.choice(string.ascii_uppercase) for _ in range(rand_len))
+            print "Input:", input
+            encrypted = encrypt(s, input)
+            print "Encrypted:", encrypted
+
+            decrypted = decrypt(s, encrypted)
+            print "Decrypted:", decrypted
+
+            self.assertTrue(decrypted == input)
 
 if __name__ == "__main__":
-    main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCryptoBox)
+    unittest.TextTestRunner(verbosity=2).run(suite)
